5c5b9c5abf533bb0fea7e20d99407b5c7df7f87d
Shawn M <litedoge111@gmail.com>
2023-02-18 12:05:04 -0800

    Update keystore.h

--- src/keystore.h
+++ src/keystore.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2009-2023 Satoshi Nakamoto
 // Copyright (c) 2009-2023 The Bitcoin developers
 // Copyright (c) 2011-2023 Litecoin Developers
+// Copyright (c) 2018-2023 The Flappycoin Developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef BITCOIN_KEYSTORE_H
